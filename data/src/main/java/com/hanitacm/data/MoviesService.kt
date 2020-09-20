ce06be3b0a633386c3be16dc4b5a346fe3d6929f
Hanitacm <ana.cordobam@gmail.com>
2020-09-20 18:04:50 +0200

    add api response class

--- data/src/main/java/com/hanitacm/data/MoviesService.kt
+++ data/src/main/java/com/hanitacm/data/MoviesService.kt
@@ -1,11 +1,14 @@
 package com.hanitacm.data
 
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
+import com.google.gson.annotations.SerializedName
+
 
 interface MoviesService {
 
     @GET("discover/movie?sort_by=popularity.desc")
-    fun getPopularMovies(@Query("api_key") apiKey: String)
+    fun getPopularMovies(@Query("api_key") apiKey: String): Single<MoviesResponse>
 
 }
